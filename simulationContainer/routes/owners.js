4cbe2132f537c3736f039e8a6a7b505b0ddc9f3e
Hammarkvast <tomham-3@student.ltu.se>
2019-12-06 13:31:27 +0100

    continued building api

--- simulationContainer/routes/owners.js
+++ simulationContainer/routes/owners.js
@@ -21,26 +21,41 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/getOwner', function(req, res, next){
-    var id = req.param.id;
-    var sql = 'SELECT * FROM owners WHERE id = ?'
-    db.query(sql, userID, function (err, row, fields) {
+    var id = req.query.id;
+    var sql = 'SELECT * FROM owners WHERE id = ?';
+    db.query(sql,[id], function (err, row, fields) {
         if(err) {
             res.status(500).send({error: 'couldnt get specific owner'});
         }
-        res.json(row[0])
+        res.json(row)
+    })
+});
+
+router.get('/getHouseElectricity', function(req, res, next){
+    var id = req.query.id;
+    var sql = 'SELECT * FROM house INNER JOIN owners ON house.houseid = owners.houseid WHERE house.houseid='+id;
+    db.query(sql, [id], function(err, row, fields){
+        if(err){
+            res.status(500).send({error: 'couldnt recieve house info'});
+        }
+        res.json(row)
     })
 });
 
 router.delete('/deleteOwner', function(req, res, next) {
     var id = req.param.id;
-    var sql = 'DELETE * FROM owners WHERE id = ?';
-    db.query(sql, userID, function(err, row, fields) {
+    var sql = 'DELETE * FROM house INNER JOIN owners ON house.houseid = owners.houseid WHERE house.houseid='+id;
+    db.query(sql, [ID], function(err, row, fields) {
         if(err){
             res.status(500).send({error: 'couldnt delete the specific owner'});
         }
-        res.json(row[0])
+        res.json(row)
     })
 })
+
+
+
+
 
 
 
