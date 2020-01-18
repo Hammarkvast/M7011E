7608aada6187372b51202959a5251766d8144fe3
Grumme2 <agh@live.se>
2020-01-18 15:21:00 +0100

    some minor fixes

--- simulationContainer/routes/signin.js
+++ simulationContainer/routes/signin.js
@@ -15,35 +15,39 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req,res,next){
     message = '';
-      var post  = req.body;
-      var name= post.username;
-      var pass= post.password;
-      //var sql = "INSERT INTO `users`(`first_name`,`last_name`,`mob_no`,`user_name`, `password`) VALUES ('" + fname + "','" + lname + "','" + mob + "','" + name + "','" + pass + "')";
- 
-      var sql = "SELECT * FROM owners WHERE username = "+ db.escape(name) + ";";
-      var query = db.query(sql, function(err, result) { 
-         if (err){
-            console.log("error log js: "+ err.log);
-            console.log("error message js: " + err.message);
-
-            message = err.message + err.log
-            res.render('signin', {message: message}); 
+   var post  = req.body;
+   var name= post.username;
+   var pass= post.password;
+   //var sql = "INSERT INTO `users`(`first_name`,`last_name`,`mob_no`,`user_name`, `password`) VALUES ('" + fname + "','" + lname + "','" + mob + "','" + name + "','" + pass + "')";
+   if (name){
+   var sql = "SELECT * FROM owners WHERE username = "+ db.escape(name) + ";";
+   var query = db.query(sql, function(err, result) { 
+      if (err){
+         console.log("error log js: "+ err.log);
+         console.log("error message js: " + err.message);
+   
+         message = err.message + err.log
+         res.render('signin', {message: message}); 
+      }
+      bcrypt.compare(pass, result[0].password, function(err, compareres) {
+         if(compareres == true){
+            req.session.loggedin = true;
+            req.session.username = name;
+            req.session.databaseid = result[0].ownerid;
+            res.redirect("/");
+         } else {
+            message = "Wrong username and/or password!";
+            res.render('signin',{message: message});
          }
-         bcrypt.compare(pass, result[0].password, function(err, compareres) {
-            if(compareres == true){
-               req.session.loggedin = true;
-               req.session.username = name;
-               req.session.databaseid = result[0].ownerid;
-               res.redirect("/");
-            } else {
-               message = "Wrong username and/or password!";
-               res.render('signin',{message: message});
-            }
         });
-
+      
          // message = "Succesfully! Your account has been created.";
          // res.render('signin',{message: message});
       });
+   }else{
+      message = "Wrong username and/or password!";
+      res.render('signin',{message: message});
+   }
 });
  
    
